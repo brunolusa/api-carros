2b6d2503815cc219b0299ef301dfcfae6514798d
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-31 19:44:37 -0300

    Adicionando liberação do cors para o Swagger-ui

--- src/main/java/com/lusa/apicarros/config/WebConfig.java
+++ src/main/java/com/lusa/apicarros/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.lusa.apicarros.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false).maxAge(3600);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
