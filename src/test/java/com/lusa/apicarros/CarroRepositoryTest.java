2e86a6348eabca1ac0ebc163243abe0d75f48ea4
Bruno Lusa <bruno.lusa@gmail.com>
2020-08-01 17:02:32 -0300

    Adicionando métodos do teste unitário

---
+++ src/test/java/com/lusa/apicarros/CarroRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.lusa.apicarros;
+
+import com.lusa.apicarros.entity.Carros;
+import com.lusa.apicarros.repository.CarrosRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarroRepositoryTest {
+
+    @Autowired
+    private CarrosRepository carrosRepository;
+
+    @Test
+    public void buscarTodosOsCarros(){
+        List<Carros> carrosList = carrosRepository.findAll();
+        assertThat(carrosList.size()).isEqualTo(7);
+    }
+
+}
