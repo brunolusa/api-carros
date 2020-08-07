4f398c8bed2eb8537e6ee40067622bed729cbe0c
Bruno Lusa <bruno.lusa@gmail.com>
2020-08-06 21:15:58 -0300

    Inserindo teste unitário da camada carro repository

--- src/test/java/com/lusa/apicarros/CarroRepositoryTest.java
+++ src/test/java/com/lusa/apicarros/CarroRepositoryTest.java
@@ -2,15 +2,12 @@ package com.lusa.apicarros;
 
 import com.lusa.apicarros.entity.Carros;
 import com.lusa.apicarros.repository.CarrosRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
