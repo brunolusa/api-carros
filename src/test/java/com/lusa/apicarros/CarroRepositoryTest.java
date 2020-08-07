e074ad131bdc67711d90fef61058e8be93127167
Bruno Lusa <bruno.lusa@gmail.com>
2020-08-06 21:33:18 -0300

    Adicionando testes dos demais métodos da controller

--- src/test/java/com/lusa/apicarros/CarroRepositoryTest.java
+++ src/test/java/com/lusa/apicarros/CarroRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
+import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -22,5 +23,27 @@ public class CarroRepositoryTest {
         List<Carros> carrosList = carrosRepository.findAll();
         assertThat(carrosList.size()).isEqualTo(7);
     }
+    @Test
+    public void buscarCarroPorId(){
+        Optional<Carros> carro = carrosRepository.findById(1L);
+        assertThat(carro.get().getMarca().equals("Ford"));
+        assertThat(carro.get().getModelo().equals("Focus"));
+    }
+    @Test
+    public void deleteCarroPorId(){
+        carrosRepository.deleteById(2L);
+    }
+    @Test
+    public void postCarro(){
+        Carros c = new Carros();
 
+        c.setMarca("Teste");
+        c.setModelo("Teste");
+        c.setId(99L);
+
+
+        Carros novoCarro = carrosRepository.save(c);
+        assertThat(novoCarro.getMarca().equals("Teste"));
+        assertThat(novoCarro.getModelo().equals("Teste"));
+    }
 }
