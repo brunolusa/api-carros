f8d2578c56331bbdf053df277c68d019c0cb0e91
Bruno Lusa <bruno.lusa@gmail.com>
2021-03-30 20:30:39 -0300

    Refatorando para não autencicar o acesso a documentação da api

--- src/main/java/com/lusa/apicarros/config/SegurancaConfig.java
+++ src/main/java/com/lusa/apicarros/config/SegurancaConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
